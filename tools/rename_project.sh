8a01f35dbd284e8b61dadd3e330a55a361a2479e
robin <robin.prillwitz@gmail.com>
2021-02-22 10:11:06 +0100

    remove -e files

--- tools/rename_project.sh
+++ tools/rename_project.sh
@@ -9,3 +9,6 @@ fi
 sed -i -e 's/project\((.*)\)$/project('${1}')/g' "CMakeLists.txt"  > /dev/null
 sed -i -e 's/PROJECT_NAME\ :=.*$/PROJECT_NAME := '${1}'/g' "Makefile"  > /dev/null
 sed -i -e '1 s/^# .*/# '${1}'/g' "README.md" > /dev/null
+
+rm ./*.*-e
+rm ./*-e
